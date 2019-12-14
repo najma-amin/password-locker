aede757007bdc207cb883c52b315a651cc9b5413
najmaA <najmaamin10@gmai.com>
2019-12-14 23:27:51 +0300

    password if and else statement generated

--- password.py
+++ password.py
@@ -1,4 +1,9 @@
-numbers = [1,2,3,4,5]
 
-for number in numbers:
-    print(number)
+#let's make a simple password prompting system
+# allow user to type in something, save it to the variables called "passwprd"
+password = input("what is the passworda")
+#determine if the password is correct
+if password == "abc123":
+    print("ACCCESS GRANTED")
+else:
+    print ("ACCESS DENIED")
