4f4fbc4b7bcabac845b0c623fcca75020f0f94bd
najmaA <najmaamin10@gmai.com>
2019-12-16 10:54:26 +0300

    created credential files

--- password_test.py
+++
@@ -1,6 +0,0 @@
-user_= input ("Enter your user ID (press 1 to create account):")
-if "1"  in user_id:
-    email= ("Enter your user ID:")
-    
-    with open ("password\\email.txt,""w")as f:
-        store_email=f.write(email)
